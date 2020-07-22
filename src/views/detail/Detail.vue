f91ab749795fe30a913603ab106485ec26ae4021
shuangbai <shuang.bai@yourdream.cc>
2020-07-22 20:16:43 +0800

    style: 商品详情页 数据整合 dec 详情图片展示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
-    <detail-nav-bar/>
-    <better-scroll class="content">
+  <div id="detail">
+    <detail-nav-bar class="detail-nav-bar"/>
+    <better-scroll class="content" ref="scroll">
         <detail-swiper :top-images="topImages"/>
         <detail-base-info :goods="goods"></detail-base-info>
         <detail-shop-info :shop="shop"></detail-shop-info>
+        <detail-goods-info :detailInfo="detailInfo" @imageLoad="imageLoad"/>
+        <detail-param-info :paramInfo="paramInfo" @imageLoad="imageLoad"/>
+
     </better-scroll>
   </div>
 </template>
@@ -14,10 +17,12 @@ import DetailNavBar from './childComps/DetailNavBar'
 import DetailSwiper from './childComps/DetailSwiper'
 import DetailBaseInfo from './childComps/DetailBaseInfo'
 import DetailShopInfo from './childComps/DetailShopInfo'
+import DetailGoodsInfo from './childComps/DetailGoodsInfo'
+import DetailParamInfo from './childComps/DetailParamInfo'
 
 import BetterScroll from 'components/common/betterscroll/BetterScroll'
 
-import {getDetail, Goods, Shop} from 'network/detail'
+import {getDetail, Goods, Shop, GoodsParam} from 'network/detail'
 
 export default {
   name: "Detail",
@@ -29,7 +34,9 @@ export default {
       DetailSwiper,
       BetterScroll,
       DetailBaseInfo,
-      DetailShopInfo
+      DetailShopInfo,
+      DetailGoodsInfo,
+      DetailParamInfo
   },
 
   data() {
@@ -37,33 +44,53 @@ export default {
       iid: '',
       topImages:[],
       goods: {},
-      shop:{}
+      shop:{},
+      detailInfo:{},
+      paramInfo:{}
     }
   },
   created() {
     this.iid = this.$route.query.iid
     getDetail(this.iid).then(res=>{
-    console.log("-----------");
-    console.log(res);
-    const data = res.result
-    //获取顶部轮播图
+      console.log(res);
+      // 1.获取数据
+      const data = res.result
+      // 2.获取顶部轮播图
       this.topImages = data.itemInfo.topImages
+      // 3.获取商品信息
       this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
+      // 4.获取店铺信息
       this.shop = new Shop(data.shopInfo)
+      // 5.获取商品详细信息
+      this.detailInfo = data.detailInfo
+      // 6.
+      this.paramInfo = new GoodsParam(data.itemParams.info,data.itemParams.rule)
+
     }).catch(err=>{
       console.log(err);
     })
+  },
+  methods: {
+    imageLoad(){
+      this.$refs.scroll.refresh()
+    }
   }
 };
 </script>
    
 <style scoped>
+  #detail{
+    position: relative;
+    z-index: 10;
+    background-color: #fff;
+    height: 100vh;
+  }
+  .detail-nav-bar{
+    position: relative;
+    z-index: 10;
+    background-color: #fff;
+  }
   .content {
-    position: absolute;
-    top: 49px;
-    left: 0;
-    right: 0;
-    bottom: 44px;
-    overflow: hidden;
+    height: calc(100% - 44px);
   }
 </style> 
